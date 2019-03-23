dc077e38721c72854ef7ea70d1b37748fa03af89
Troy Gooden <tro12185@gmail.com>
2019-03-23 18:03:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihirio_matsumoto]= {}
-programmer_hash[:yukihirio_matsumoto]
+programmer_hash[:yukihiro_matsumoto]= {}
+programmer_hash[:yukihiro_matsumoto]
     
 end
 
